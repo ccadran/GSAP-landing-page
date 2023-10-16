57d8f94fec74e759ee6afe96e08fa0c4371929fb
ccadran <clariocadran33@gmail.com>
2023-10-16 21:36:07 +0200

    Intro component

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,17 +1,51 @@
 import styles from "./style.module.css";
 import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef } from "react";
 
 export default function index() {
+  const backgroundImage = useRef(null);
+  const introImage = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: "top",
+        end: "+=500px",
+        scrub: true,
+        markers: true,
+      },
+    });
+    timeline
+      .fromTo(
+        backgroundImage.current,
+        { clipPath: `inset(15%)` },
+        { clipPath: `inset(0%)` }
+      )
+      .to(introImage.current, { height: "200px" }, 0);
+  }, []);
   return (
     <div className={styles.intro}>
-      <div className={styles.backgroundImage}>
+      <div ref={backgroundImage} className={styles.backgroundImage}>
         <Image src={"/images/background.jpeg"} fill={true} />
       </div>
+
       <div className={styles.introContainer}>
-        <div className={styles.introImage}>
+        <div
+          ref={introImage}
+          data-scroll
+          data-scroll-speed="0.3"
+          className={styles.introImage}
+        >
           <Image src={"/images/intro.png"} fill={true} />
         </div>
-        <h1>Smooth Scroll</h1>
+        <h1 data-scroll data-scroll-speed="0.7">
+          Smooth Scroll
+        </h1>
       </div>
     </div>
   );
